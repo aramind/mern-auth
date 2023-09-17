09039aa302a65ccbd7881bd080b5fadb16054d6c
aramind <r.m.miranda.ece@gmail.com>
2023-09-17 22:08:10 +0800

    add routes and create pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,38 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Signin from "./pages/Signin";
+import SIgnup from "./pages/SIgnup";
+import Profile from "./pages/Profile";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<Home />}
+        />
+        <Route
+          path="/about"
+          element={<About />}
+        />
+        <Route
+          path="/sign-in"
+          element={<Signin />}
+        />
+        <Route
+          path="/sign-up"
+          element={<SIgnup />}
+        />
+        <Route
+          path="/profile"
+          element={<Profile />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ client/src/pages/SIgnup.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SIgnup = () => {
+  return <div>SIgnup</div>;
+};
+
+export default SIgnup;
