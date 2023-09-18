6f01c0b6c18f35996aeaa6a21efd4679f45676ba
aramind <r.m.miranda.ece@gmail.com>
2023-09-18 22:55:02 +0800

    creating sign up API route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,20 @@
+const User = require("../models/user.model");
+const bcryptjs = require("bcryptjs");
+
+const authController = {
+  signup: async (req, res) => {
+    const { username, email, password } = req.body;
+
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+
+    const newUser = new User({ username, email, password: hashedPassword });
+    try {
+      await newUser.save();
+      res.status(201).json({ message: "User created successfully" });
+    } catch (error) {
+      res.status(500).json(error.message);
+    }
+  },
+};
+
+module.exports = authController;
--- api/index.js
+++ api/index.js
@@ -2,13 +2,18 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoutes = require("./routes/user.route");
+const authRoutes = require("./routes/auth.route");
 dotenv.config();
 
 const app = express();
 const PORT = 3001;
 
+// middlewares
+app.use(express.json());
+
 // Routers
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
 mongoose
   .connect(process.env.MONGO)
