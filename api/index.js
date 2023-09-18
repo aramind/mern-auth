c4b6c4a3e0428ecbf8fec0c64c00643c887f5629
aramind <r.m.miranda.ece@gmail.com>
2023-09-18 11:01:29 +0800

    feat: connecting to mongoDB

--- api/index.js
+++ api/index.js
@@ -1,5 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => console.log(err));
 const app = express();
 
 const PORT = 3001;
